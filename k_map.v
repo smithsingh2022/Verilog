b5a52c2a26db2f7c5426461990005aa226ecae40
Smith Singh <73015240+smithsingh2022@users.noreply.github.com>
2020-10-17 13:55:28 +0530

    Create k_map.v

---
+++ k_map.v
@@ -0,0 +1,9 @@
+// k-map implementation
+module k_map (Y,A,B,C,D);       // module declaration   8062
+
+output Y;                       // output
+input A,B,C,D;                  // input
+
+assign Y = ((!B)&&(!C)) || ((!A)&&C&&(!D)) ;  // Y = A'CD' + B'C'
+
+endmodule;       // end of module
