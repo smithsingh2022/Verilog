46a62d12c9ac4fc3de780764c242cad0b29ab6a1
2015jamrajput <92567841+2015jamrajput@users.noreply.github.com>
2021-10-15 12:43:47 +0530

    Update k_map.v

--- k_map.v
+++ k_map.v
@@ -4,6 +4,6 @@ module k_map (Y,A,B,C,D);       // module declaration   8062
 output Y;                       // output
 input A,B,C,D;                  // input
 
-assign Y = ((!B)&&(!C)) || ((!A)&&C&&(!D)) ;  // Y = A'CD' + B'C'
+  assign Y = ((!B)||(!C)) || ((!A)&&C&&(!D)) ;  // Y = A'CD' + B'C'
 
 endmodule;       // end of module
